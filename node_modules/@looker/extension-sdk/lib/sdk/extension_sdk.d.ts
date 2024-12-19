76d800595718be92bedbb92c012c257067fdc3b8
eran-jutomate <eran@jutomate.com>
2024-12-19 13:45:33 +0200

    Delete node_modules directory

--- node_modules/@looker/extension-sdk/lib/sdk/extension_sdk.d.ts
+++
@@ -1,6 +0,0 @@
-import { Looker40SDK } from '@looker/sdk';
-import type { ExtensionHostApi } from '../connect';
-export declare class LookerExtensionSDK {
-    static create40Client(hostConnection: ExtensionHostApi): Looker40SDK;
-    static createClient(hostConnection: ExtensionHostApi): Looker40SDK;
-}
